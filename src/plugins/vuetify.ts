e839d2636b7c6f8e61023e48ff213c7c7e5acf95
A-tak <hoge+git@a-tak.com>
2020-05-04 15:55:29 +0900

    vuetifyのcssが適用されない問題への対応

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -2,6 +2,7 @@ import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import colors from 'vuetify/lib/util/colors'
+import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify)
 
