33b9b13a68acaed5e9945e678d32b49dd9f05d7c
A-tak <hoge+git@a-tak.com>
2020-05-20 20:16:35 +0900

    closed #62 ポップアップにアイコン追加

--- src/popup/router/pages/Index.vue
+++ src/popup/router/pages/Index.vue
@@ -2,9 +2,14 @@
   <div class="d-flex flex-column ma-4">
     <span class="title">{{ $t("message.page_title") }}</span>
     <v-card class="ma-2">
+      <v-avatar class="ma-3" size="125" tile>
+        <v-img src="../../../../icons/icon_128.png"></v-img>
+      </v-avatar>
       <v-card-text>{{ $t("message.page_head_content") }}</v-card-text>
       <v-card-actions>
-        <v-btn text color="accent" @click="openLearnMore">{{ $t("message.learn_more") }}</v-btn>
+        <v-btn text color="accent" @click="openLearnMore">{{
+          $t("message.learn_more")
+        }}</v-btn>
       </v-card-actions>
     </v-card>
   </div>
@@ -16,7 +21,7 @@ import { Component, Vue } from "vue-property-decorator"
 @Component
 export default class App extends Vue {
   private openLearnMore() {
-    browser.tabs.create({url: this.$t("message.learn_more_url").toString() })
+    browser.tabs.create({ url: this.$t("message.learn_more_url").toString() })
   }
 }
 </script>
