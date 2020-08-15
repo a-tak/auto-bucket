303dd54e1d9690ca1f77bf862650ddb16df9cccb
A-tak <hoge+git@a-tak.com>
2020-08-15 07:03:48 +0000

    コメント削除

--- webpack.config.js
+++ webpack.config.js
@@ -133,28 +133,6 @@ const config = {
         },
       ],
     }),
-    // new CopyPlugin([
-    //   { from: 'icons', to: 'icons', ignore: ['icon.xcf'] },
-    //   { from: 'popup/popup.html', to: 'popup/popup.html', transform: transformHtml },
-    //   { from: 'options/options.html', to: 'options/options.html', transform: transformHtml },
-    //   { from: 'logviewer/logviewer.html', to: 'logviewer/logviewer.html', transform: transformHtml },
-    //   { from: 'statistics/statistics.html', to: 'statistics/statistics.html', transform: transformHtml },
-    //   { from: '_locales', to: '_locales', transform: transformHtml },
-    //   {
-    //     from: 'manifest.json',
-    //     to: 'manifest.json',
-    //     transform: content => {
-    //       const jsonContent = JSON.parse(content);
-    //       jsonContent.version = version;
-
-    //       if (config.mode === 'development') {
-    //         jsonContent['content_security_policy'] = "script-src 'self' 'unsafe-eval'; object-src 'self'";
-    //       }
-
-    //       return JSON.stringify(jsonContent, null, 2);
-    //     },
-    //   },
-    // ]),
   ],
 }
 
